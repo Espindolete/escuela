aa9ec29e757ce44ebec272c2648aed86c91c1705
MeDicenAgus <agustinespin@hotmail.com>
2018-11-22 00:17:29 -0300

    update

--- server.js
+++ server.js
@@ -24,18 +24,22 @@ db.connect((err)=>{
   console.log('MySql Connected...');
 });
 
-
-//ready
-app.get('/getMateria/:id',(req,res)=>{
-  let sql =`select * from materias where id = ${req.params.id}`;
+app.get('/getProfesores',(req,res)=>{
+  let sql='select * from profesores';
   db.query(sql,(err,result)=>{
-    if(err) throw err;
-    //console.log(result);
+    if(err) console.log(err);
     res.send(result);
   })
-});
+})
 
-//ready
+app.get('/getPreceptores',(req,res)=>{
+  let sql='select * from preceptores';
+  db.query(sql,(err,result)=>{
+    if(err) console.log(err);
+    res.send(result);
+  })
+})
+
 app.get('/getMaterias',(req,res)=>{
   let sql='select * from materias ';
   db.query(sql,(err,result,fields)=>{
@@ -45,27 +49,33 @@ app.get('/getMaterias',(req,res)=>{
   })
 });
 
-//ready
-app.get('/getCursoMat/:id',(req,res)=>{
-  let sql=`select * from cursos where ${req.params.id} in (M1,M2)`;
-  db.query(sql,(err,result,fields)=>{
+
+app.get('/getMateria/:id',(req,res)=>{
+  let sql =`select * from materias where id = ${req.params.id}`;
+  db.query(sql,(err,result)=>{
     if(err) throw err;
     //console.log(result);
     res.send(result);
   })
 });
 
-//ready
-app.post('/postNota',(req,res)=>{
-  req
-  let sql=`insert into notas set ?`
-  let query= db.query(sql,req.body,(err,resul)=>{
-    if(err)console.log(err);
-    //console.log(req.body);
-    res.send("funco");
+app.get('/getCursos',(req,res)=>{
+  let sql=`select * from cursos`;
+  db.query(sql,(err,result)=>{
+    if(err) console.log(err);
+    res.send(result);
   })
 })
 
+
+app.get('/getCursoMat/:id',(req,res)=>{
+  let sql=`SELECT cursos.id,cursos.nombre FROM cursos INNER JOIN cursohasmaterias ON cursohasmaterias.curso_fk = cursos.id INNER JOIN materias ON cursohasmaterias.materia_fk = materias.id where materias.id =${req.params.id}`;
+  db.query(sql,(err,result,fields)=>{
+    if(err) throw err;
+    //console.log(result);
+    res.send(result);
+  })
+});
 
 
 app.get('/getNotas/:alum/:mat',(req,res)=>{
@@ -77,15 +87,74 @@ app.get('/getNotas/:alum/:mat',(req,res)=>{
   })
 })
 
-//ready
-app.get('/getAlumnosMat/:curso',(req,res)=>{
+app.get('/getAlumnosCur/:curso',(req,res)=>{
   let sql=`select * from alumnos where curso_fk=${req.params.curso}`;
   db.query(sql,(err,result,fields)=>{
-    if(err) throw err;
-    //console.log(result);
+    if(err) console.log(err);
+    console.log(sql);
     res.send(result);
   })
 });
+
+app.get('/getMateriasProf/:prof',(req,res)=>{
+  let sql=`SELECT materias.id, materias.nombre FROM materias INNER JOIN materia_has_profe ON materia_has_profe.materia_fk = materias.id INNER JOIN profesores ON profesores.id = materia_has_profe.profe_fk where profesores.id=${req.params.prof}`;
+  db.query(sql,(err,result)=>{
+    if(err) console.log(err);
+    res.send(result);
+  })
+})
+
+////////////////POST//////////////////
+app.post('/postMateria',(req,res)=>{
+  let sql="insert into materias set ?"
+  db.query(sql,req.body,(err,result)=>{
+    if(err) console.log(err);
+    res.send(result);
+  })
+
+})
+
+app.post('postProfesor',(req,res)=>{
+  let sql='select * from profesores';
+  db.query(sql,req.body,(err,result)=>{
+    if(err) console.log(err);
+    res.send(result);
+  })
+})
+
+app.post('postPreceptor',(req,res)=>{
+  let sql='insert into preceptores set ?'
+  db.query(sql,req.body,(err,result)=>{
+    if(err) console.log(err);
+    res.send(result);
+  })
+
+})
+
+app.post('/postAlumno',(req,res)=>{
+  let sql='insert into alumnos set ?';
+  db.query(sql,req.body,(err,result)=>{
+    if(err) console.log(err);
+    res.send(result);
+  })
+})
+
+app.post('/postCurso',(req,res)=>{
+  let sql='insert into cursos set ?';
+  let query=db.query(sql,req.body,(err,result)=>{
+    if(err) console.log(err);
+    res.send(result);
+  })
+})
+
+app.post('/postNota',(req,res)=>{
+  let sql=`insert into notas set ?`;
+  let query= db.query(sql,req.body,(err,result)=>{
+    if(err)console.log(err);
+    //console.log(req.body);
+    res.send(result);
+  })
+})
 
 
 app.listen('3000',()=>{
